c3908074727caedf801cf40f7abb3448f3ee17bc
gastonTallafer <132406599+gastonTallafer@users.noreply.github.com>
2023-06-06 20:19:30 -0300

    Update punto_10.py

--- punto_10.py
+++ punto_10.py
@@ -1,27 +1,56 @@
-class Pila:
+class Notificacion:
+    def __init__(self, hora, aplicacion, mensaje):
+        self.hora = hora
+        self.aplicacion = aplicacion
+        self.mensaje = mensaje
+
+class ColaNotificaciones:
     def __init__(self):
-        self.dioses = []
+        self.notificaciones = []
 
-    def insertar_atenea(self, nombre, posicion):
-        cima = len(self.dioses) - 1
-        
-        self.dioses.append(None)  
-        for i in range(cima, posicion - 1, -1):
-            self.dioses[i + 1] = self.dioses[i]
-        
-        self.dioses[posicion] = nombre
+    def agregar_notificacion(self, notificacion):
+        self.notificaciones.append(notificacion)
 
-    def imprimir_pila(self):
-        for nombre in reversed(self.dioses):
-            print(nombre)
+    def obtener_notificacion(self):
+        if self.notificaciones:
+            return self.notificaciones.pop(0)
+        else:
+            return None
 
-pila_dioses = Pila()
+# Ejemplo de uso
+cola = ColaNotificaciones()
 
-pila_dioses.dioses.append('Zeus')
-pila_dioses.dioses.append('Poseidón')
-pila_dioses.dioses.append('Apolo')
-pila_dioses.dioses.append('Afrodita')
+# Agregar notificaciones a la cola
+notificacion1 = Notificacion("09:30", "Facebook", "Tienes una nueva solicitud de amistad")
+cola.agregar_notificacion(notificacion1)
 
-pila_dioses.insertar_atenea('Atenea', 2)
+notificacion2 = Notificacion("10:15", "Twitter", "Nuevo mensaje directo")
+cola.agregar_notificacion(notificacion2)
 
-pila_dioses.imprimir_pila()
+# Obtener y mostrar la primera notificación de la cola
+primera_notificacion = cola.obtener_notificacion()
+if primera_notificacion:
+    print("Hora:", primera_notificacion.hora)
+    print("Aplicación:", primera_notificacion.aplicacion)
+    print("Mensaje:", primera_notificacion.mensaje)
+else:
+    print("No hay notificaciones en la cola.")
+
+def eliminar_notificaciones_facebook(cola):
+    cola_sin_facebook = [notif for notif in cola if notif["app"] != "Facebook"]
+    return cola_sin_facebook
+
+def mostrar_notificaciones_twitter_python(cola):
+    notificaciones_python = [notif for notif in cola if notif["app"] == "Twitter" and "Python" in notif["mensaje"]]
+    for notif in notificaciones_python:
+        print(notif)
+
+def contar_notificaciones_entre_horas(cola):
+    pila_temporal = []
+    contador = 0
+    for notif in cola:
+        hora = notif["hora"]
+        if hora >= "11:43" and hora <= "15:57":
+            pila_temporal.append(notif)
+            contador += 1
+    return contador, pila_temporal
